45525c6bca81ddea39d6dbb0319798c454e4a80b
Brawmario <38757897+Brawmario@users.noreply.github.com>
2026-01-14 01:10:24 +0000

    Deploying to gh-pages from @ Brawmario/infinite-mario-64-ever@203d09fc2103a27266a4ce35e42aa287dfc33f3f 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1768350831|3879218';
+const CACHE_VERSION = '1768352988|3615127';
 /** @type {string} */
 const CACHE_PREFIX = 'Infinite Mario 6-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
